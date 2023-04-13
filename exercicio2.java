54b2e13311773ab1fbc0901bd968a9b6e69ee7d3
Richard Valentim <88457347+Valenart@users.noreply.github.com>
2023-04-12 22:34:31 -0300

    Update exercicio2.java

--- exercicio2.java
+++ exercicio2.java
@@ -1,6 +1,11 @@
 class Main {
   public static void main(String[] args) {
     
+    /*
+    FAÇA UM PROGRAMA SIMPLES, ONDE IMPRIMA A MÉDIA ARITMÉTICA DOS NÚMEROS 8,9 E 7.
+    A MÉDIA DOS NÚMEROS 4, 5 E 6. A SOMA DAS DUAS MÉDIAS. A MÉDIA DAS MÉDIAS.
+    */
+    
     int grupo1 = ((7 + 8 + 9)/3);
     System.out.println("A média do grupo 1 (7,8 e 9) será: " + grupo1);
     
