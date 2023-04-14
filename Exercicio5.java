1e3bd80f23a13729f66d29a903eb6581d22774f9
Richard Valentim <88457347+Valenart@users.noreply.github.com>
2023-04-13 21:09:59 -0300

    Create Exercicio5.java

---
+++ Exercicio5.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+import java.util.Random;
+class Main {
+  public static void main(String[] args) {
+
+   //SCANNER
+    Scanner sc = new Scanner(System.in); 
+    Random rand = new Random(); 
+    
+    
+    int chute = 0; 
+    int numeroRandom = rand.nextInt(101);
+    
+    
+    
+    System.out.println("ACERTE O NUMERO QUE ESTOU PENSANDO DE 0 A 100!");
+    do{  
+       System.out.println("Chute um numero: ");
+       chute = sc.nextInt();    
+       
+        if (chute >= 101 || chute <0 ){
+        System.out.println("Chute dentro do alcance que citei!");
+        }
+       
+        else if (chute > numeroRandom){
+             if (chute - numeroRandom <= 5){
+                System.out.println("Foi quase! Tente novamente!");
+            }
+             else {
+                System.out.println("Chute mais baixo! Tente de novo: ");
+            }
+        }
+        else if (numeroRandom > chute){
+            if (numeroRandom - chute <= 5){
+            System.out.println("Foi quase! Tente novamente!");
+            }
+            else {
+                System.out.println("Chute mais alto! Tente de novo:");
+            }
+        }
+             
+   } while (chute != numeroRandom);
+    
+    System.out.println("Parabéns, você acertou!");
+  }
+}
