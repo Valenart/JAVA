082c8875b773aa49c173ea77ed98acf405848801
Richard Valentim <88457347+Valenart@users.noreply.github.com>
2023-04-13 21:15:22 -0300

    Update Exercicio5.java

--- Exercicio5.java
+++ Exercicio5.java
@@ -1,46 +1,43 @@
 import java.util.Scanner;
 import java.util.Random;
+
 class Main {
   public static void main(String[] args) {
 
-   //SCANNER
-    Scanner sc = new Scanner(System.in); 
-    Random rand = new Random(); 
-    
-    
-    int chute = 0; 
+    // SCANNER
+    Scanner sc = new Scanner(System.in);
+    Random rand = new Random();
+
+    int chute = 0;
     int numeroRandom = rand.nextInt(101);
-    
-    
-    
+    int palpite = 0;
+
     System.out.println("ACERTE O NUMERO QUE ESTOU PENSANDO DE 0 A 100!");
-    do{  
-       System.out.println("Chute um numero: ");
-       chute = sc.nextInt();    
-       
-        if (chute >= 101 || chute <0 ){
+    do {
+      System.out.println("Chute um numero: ");
+      chute = sc.nextInt();
+
+      if (chute >= 101 || chute < 0) {
         System.out.println("Chute dentro do alcance que citei!");
+      }
+
+      else if (chute > numeroRandom) {
+        if (chute - numeroRandom <= 5) {
+          System.out.println("Foi quase! Tente novamente!");
+        } else {
+          System.out.println("Chute mais baixo! Tente de novo: ");
         }
-       
-        else if (chute > numeroRandom){
-             if (chute - numeroRandom <= 5){
-                System.out.println("Foi quase! Tente novamente!");
-            }
-             else {
-                System.out.println("Chute mais baixo! Tente de novo: ");
-            }
+      } else if (numeroRandom > chute) {
+        if (numeroRandom - chute <= 5) {
+          System.out.println("Foi quase! Tente novamente!");
+        } else {
+          System.out.println("Chute mais alto! Tente de novo:");
         }
-        else if (numeroRandom > chute){
-            if (numeroRandom - chute <= 5){
-            System.out.println("Foi quase! Tente novamente!");
-            }
-            else {
-                System.out.println("Chute mais alto! Tente de novo:");
-            }
-        }
-             
-   } while (chute != numeroRandom);
-    
-    System.out.println("Parabéns, você acertou!");
+      }
+      palpite++;
+    } while (chute != numeroRandom);
+
+    System.out.println("Parabéns, você acertou com " + palpite
+        + " tentativas!");
   }
 }
